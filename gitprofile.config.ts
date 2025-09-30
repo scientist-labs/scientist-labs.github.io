2f8c88997ecc536855d43ce707ca99568f4d3e09
Chris Petersen <chris@petersen.io>
2025-09-30 16:26:48 -0700

    Adding the patent

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -83,15 +83,15 @@ const CONFIG = {
   certifications: [],
   educations: [],
   publications: [
-    // {
-    //   title: 'Publication Title',
-    //   conferenceName: '',
-    //   journalName: 'Journal Name',
-    //   authors: 'John Doe, Jane Smith',
-    //   link: 'https://example.com',
-    //   description:
-    //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    // },
+    {
+      title: 'Decentralized data verification',
+      conferenceName: '',
+      journalName: 'Patent Number: PCT/US2019/029227',
+      authors: 'Chris Petersen',
+      link: 'https://patents.google.com/patent/WO2020036657A1/en',
+      description:
+        'Provided herein are methods, systems, and media that leverage blockchain technology and cryptography implemented on decentralized or distributed peer-to-peer networks to provide reliable and secure verification of data integrity.',
+    },
     // {
     //   title: 'Publication Title',
     //   conferenceName: 'Conference Name',
